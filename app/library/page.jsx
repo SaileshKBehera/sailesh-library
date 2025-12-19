4767217affbee4265d8faf1b1d70f6655f3beeb7
Sailesh Behera <sipu.kkc@gmail.com>
2025-12-19 12:10:50 +0530

    Fix

---
+++ app/library/page.jsx
@@ -0,0 +1,9 @@
+export default function LibraryPage() {
+  return (
+    <main>
+      <h1>TanviTech</h1>
+      <p>All topics are publicly readable.</p>
+      {/* Render topics here */}
+    </main>
+  )
+}
