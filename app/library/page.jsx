6c3920c09c6cb1c1edf376bfabdb6dde95b56a3b
Sailesh Behera <sipu.kkc@gmail.com>
2025-12-19 10:11:48 +0530

    Fixes

---
+++ app/library/page.jsx
@@ -0,0 +1,9 @@
+export default function LibraryPage() {
+  return (
+    <main>
+      <h1>Knowledge Library</h1>
+      <p>All topics are publicly readable.</p>
+      {/* Render topics here */}
+    </main>
+  )
+}
