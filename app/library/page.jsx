42da2b599f2ece2c1d82de6599d91989f14c52b2
Sailesh Behera <sipu.kkc@gmail.com>
2025-12-19 10:40:27 +0530

    Revert "Fixes"
    This reverts commit 6c3920c09c6cb1c1edf376bfabdb6dde95b56a3b.

--- app/library/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function LibraryPage() {
-  return (
-    <main>
-      <h1>Knowledge Library</h1>
-      <p>All topics are publicly readable.</p>
-      {/* Render topics here */}
-    </main>
-  )
-}
